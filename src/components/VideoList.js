7cdeaf7ae6b1bef53100366d4a011e89d7007156
Shubham Khandelwal <beeshub7@gmail.com>
2021-06-06 02:31:06 +0530

    -Create videos app with React class-based components -Refactor SearchBar component to use hooks

---
+++ src/components/VideoList.js
@@ -0,0 +1,11 @@
+import VideoItem from "./VideoItem.js";
+
+const VideoList = ({videos, onVideoSelect}) => {
+    const renderedList = videos.map(video => <VideoItem key={video.id.videoId} video={video} onVideoSelect={onVideoSelect}/>);
+
+    return (
+        <div className="ui relaxed divided list">{renderedList}</div>
+    );
+};
+
+export default VideoList;
